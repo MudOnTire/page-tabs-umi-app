2f1d189022f31f8fbdab348db1330044cc0f3afd
13851731474 <13851731474@localhost>
2020-07-21 17:58:42 +0800

    add customize context menu label demo

--- src/components/PageTab/TabLayout.tsx
+++ src/components/PageTab/TabLayout.tsx
@@ -1,3 +1,13 @@
+import React from 'react';
 import { TabLayout } from 'antd-pro-page-tabs';
 
-export default TabLayout;
+const contextMenuLabels = {
+  closeTab: '关闭标签',
+  closeRightTabs: '关闭右侧标签',
+  closeAllTabs: '关闭所有标签',
+};
+
+export default (props: any) => {
+  const { children } = props;
+  return <TabLayout {...props} contextMenuLabels={contextMenuLabels} />;
+};
